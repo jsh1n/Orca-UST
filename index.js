4f9e854b1b7707d810e43168593c40a46017d991
jsh1n <jsh1n.idle@gmail.com>
2022-03-03 00:36:40 +0900

    add solend

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Connection, PublicKey } = require("@solana/web3.js");
+const { TokenListProvider, TokenInfo } = require("@solana/spl-token-registry");
 const { getOrca, OrcaFarmConfig, OrcaPoolConfig } = require("@orca-so/sdk")
-const { SolendAction, SolendMarket } = require("@solendprotocol/solend-sdk");
-
+const { SolendMarket } = require("@solendprotocol/solend-sdk");
 
 const { google } = require("googleapis");
 
-const appendSheet = (rows) => {
+const rpcEndpoint = process.env.NODERPC_ENDPOINT
+const ownerPubkey = process.env.OWNER_PUBKEY
+
+const appendSheet = (range, rows) => {
   const scopes = ['https://www.googleapis.com/auth/spreadsheets'];
   const spreadsheetId = process.env.SPREADSHEET_ID
   return google.auth.getClient({
@@ -16,7 +19,7 @@ const appendSheet = (rows) => {
           return sheets.spreadsheets.values.append({
               auth,
               spreadsheetId,
-              range: "Sheet1!A2",
+              range,
               valueInputOption: "RAW",
               insertDataOption: "INSERT_ROWS",
               resource: {
@@ -27,14 +30,48 @@ const appendSheet = (rows) => {
 }
 
 
-const getOrcaData = async () => {
-  const rpcEndpoint = process.env.NODERPC_ENDPOINT
-  const ownerPubkey = process.env.OWNER_PUBKEY
 
-  const connection = new Connection(rpcEndpoint, "singleGossip");
+const getSolendData = async (conn, pubkey) => {
+  const market = await SolendMarket.initialize(
+    conn,
+    'production'
+  );
+  const pubkey = new PublicKey(pubkey)
+  const obligation = await market.fetchObligationByWallet(pubkey);
 
-  const orca = getOrca(connection);
-  const pubkey = new PublicKey(ownerPubkey)
+  const tokensMap = new Map();
+  const tokenList = await new TokenListProvider().resolve().then((tokens) => {
+      const tokenList = tokens.filterByClusterSlug('mainnet-beta').getList();
+      return tokenList.forEach(t => tokensMap.set(t.address, t))
+  })
+
+  const deposits = obligation.deposits.map((d) => {
+      const s = d.amount.toString()
+      const tokenInfo = tokensMap.get(d.mintAddress)
+      const balance = s.slice(0, s.length-tokenInfo.decimals) + '.' + s.slice(s.length - tokenInfo.decimals)
+      return {
+        symbol: tokenInfo.symbol,
+        balance: parseFloat(balance),
+      }
+  })
+  const borrows = obligation.borrows.map((b) => {
+      const s = b.amount.toString()
+      const tokenInfo = tokensMap.get(b.mintAddress)
+      const balance = s.slice(0, s.length-tokenInfo.decimals) + '.' + s.slice(s.length - tokenInfo.decimals)
+      return {
+        symbol: tokenInfo.symbol,
+        balance: parseFloat(balance),
+      }
+  })
+  return {
+      deposits: deposits,
+      borrows: borrows
+  }
+}
+
+
+const getOrcaData = async (conn, pubkey) => {
+  const orca = getOrca(conn);
 
   const solUsdcAq = orca.getFarm(OrcaFarmConfig.SOL_USDC_AQ)
   const solUsdcPool = orca.getPool(OrcaPoolConfig.SOL_USDC)
@@ -60,7 +97,14 @@ const getOrcaData = async () => {
 }
 
 exports.main = async () => {
-    return getOrcaData().then(data => {
-        appendSheet([[data.timestamp, data.maxPoolTokenAmountIn, data.minTokenAOut, data.minTokenBOut, data.constantProduct, data.unclaimedOrca]]).then(console.log)
+    const pubkey = new PublicKey(process.env.OWNER_PUBKEY)
+    const conn = new Connection(process.env.NODERPC_ENDPOINT, {commit: 'finalized'})
+    getOrcaData(conn, pubkey).then(data => {
+        appendSheet('Sheet1!A2', [[data.timestamp, data.maxPoolTokenAmountIn, data.minTokenAOut, data.minTokenBOut, data.constantProduct, data.unclaimedOrca]]).then(console.log)
+    })
+    getSolendData(conn, pubkey).then(data => {
+        const ustdeposit = data.deposits.find(d => d.symbol == "UST")
+        const ustborrow = data.borrows.find(b => b.symbol == "SOL")
+        appendSheet('Sheet2!A2', [[ustdeposit.balance, solborrow.balance]]).then(console.log)
     })
 };
