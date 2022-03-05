0c62fd41c389873178f795bd3614950a95a3ff73
jsh1n <jsh1n.idle@gmail.com>
2022-03-06 01:57:58 +0900

    fix

--- index.js
+++ index.js
@@ -104,10 +104,10 @@ exports.main = async () => {
       promises.push(getSolendData(conn, pubkey));
       return Promise.all(promises).then(([blocktime, orcaData, solendData]) => {
         const ps = []
-        appendSheet('Sheet1!A2', [[slot, blocktime, orcaData.maxPoolTokenAmountIn, orcaData.minTokenAOut, orcaData.minTokenBOut, orcaData.constantProduct, orcaData.unclaimedOrca]]));
+        appendSheet('Sheet1!A2', [[slot, blocktime, orcaData.maxPoolTokenAmountIn, orcaData.minTokenAOut, orcaData.minTokenBOut, orcaData.constantProduct, orcaData.unclaimedOrca]]);
         const ustdeposit = solendData.deposits.find(d => d.symbol == "UST")
         const solborrow = solendData.borrows.find(b => b.symbol == "SOL")
-        appendSheet('Sheet2!A2', [[slot, blocktime, ustdeposit.balance, solborrow.balance]])
+        appendSheet('Sheet2!A2', [[slot, blocktime, ustdeposit.balance, solborrow.balance]]);
         }));
         return Promise.all(ps).then(console.log);
         })
